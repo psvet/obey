351d40a9750e9186b34985ae084b8e84348ef9fd
fluidbyte <kent@fluidbyte.net>
2016-02-22 16:52:53 -0500

    Adds url validation type

---
+++ src/types/url.js
@@ -0,0 +1,6 @@
+export default context => {
+  const urlRegEx = /^((http|https|ftp):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+  if (!urlRegEx.test(context.value)) {
+    context.fail('Value must be a valid URL')
+  }
+}
---
+++ test/src/types/url.spec.js
@@ -0,0 +1,21 @@
+/* global describe, it, expect, sinon */
+import url from 'src/types/url'
+
+describe('type:url', () => {
+  it('calls context.fail if type is not a valid URL', () => {
+    const context = {
+      value: 'foo',
+      fail: sinon.spy()
+    }
+    url(context)
+    expect(context.fail).to.be.calledWith('Value must be a valid URL')
+  })
+  it('does not call context.fail if type is a valid URL', () => {
+    const context = {
+      value: 'https://www.google.com/test',
+      fail: sinon.spy()
+    }
+    url(context)
+    expect(context.fail).to.not.be.called
+  })
+})

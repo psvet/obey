64807974d8b071c417d872900a00000196b52e6f
fluidbyte <kent@fluidbyte.net>
2016-04-07 11:28:08 -0400

    Adds noproto subtype to url, issue #43

--- src/typeStrategies/url.js
+++ src/typeStrategies/url.js
@@ -1,9 +1,15 @@
 const url = {
   _regex: {
-    default: /^((http|https|ftp):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+    default: /^((http|https|ftp):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
+    noproto: /^([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
   },
   default: context => {
     if (context.value === null || context.value && !context.value.toString().match(url._regex.default)) {
+      context.fail('Value must be a valid URL')
+    }
+  },
+  noproto: context => {
+    if (context.value === null || context.value && !context.value.toString().match(url._regex.noproto)) {
       context.fail('Value must be a valid URL')
     }
   }
--- test/src/typeStrategies/url.spec.js
+++ test/src/typeStrategies/url.spec.js
@@ -1,8 +1,8 @@
 /* global describe, it, expect, sinon */
 import url from 'src/typeStrategies/url'
 
-describe('type:url', () => {
-  it('calls context.fail if type is not a valid URL', () => {
+describe.only('type:url', () => {
+  it('calls context.fail if type is not a valid URL (default)', () => {
     const context = {
       value: 'foo',
       fail: sinon.spy()
@@ -10,12 +10,28 @@ describe('type:url', () => {
     url.default(context)
     expect(context.fail).to.be.calledWith('Value must be a valid URL')
   })
-  it('does not call context.fail if type is a valid URL', () => {
+  it('calls context.fail if type is not a valid URL (noproto)', () => {
+    const context = {
+      value: 'foo',
+      fail: sinon.spy()
+    }
+    url.noproto(context)
+    expect(context.fail).to.be.calledWith('Value must be a valid URL')
+  })
+  it('does not call context.fail if type is a valid URL (default)', () => {
     const context = {
       value: 'https://www.google.com/test',
       fail: sinon.spy()
     }
     url.default(context)
+    expect(context.fail).to.not.be.called
+  })
+  it('does not call context.fail if type is a valid URL (noproto)', () => {
+    const context = {
+      value: 'www.google.com/test',
+      fail: sinon.spy()
+    }
+    url.noproto(context)
     expect(context.fail).to.not.be.called
   })
 })

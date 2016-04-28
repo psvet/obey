da0a7c42ac7088d4c12e665efd6f4d5aa1d626df
fluidbyte <kent@fluidbyte.net>
2016-04-27 20:28:10 -0400

    Use more permissive email regex in strategy, issue #46

--- src/typeStrategies/email.js
+++ src/typeStrategies/email.js
@@ -1,6 +1,6 @@
 const email = {
   _regex: {
-    default: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    default: /.+@.+\..+/
   },
   default: context => {
     if (context.value === null || context.value && !context.value.toString().match(email._regex.default)) {

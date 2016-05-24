dcb19ef5aaa9cecb689bae83f7a2923d7e61c4d8
Kent Safranski <kent@fluidbyte.net>
2016-05-24 09:47:45 -0500

    Fix regex to allow numbers in domains

--- src/typeStrategies/url.js
+++ src/typeStrategies/url.js
@@ -1,6 +1,6 @@
 const url = {
   _regex: {
-    default: /^(?:https?:\/\/)?[^\s\/\.]+(?:\.[a-z]{2,})+(?:\/\S*)?$/i
+    default: /^(?:https?:\/\/)?[^\s\/\.]+(?:\.[a-z0-9]{2,})+(?:\/\S*)?$/i
   },
   default: context => {
     if (context.value === null || context.value && !context.value.toString().match(url._regex.default)) {

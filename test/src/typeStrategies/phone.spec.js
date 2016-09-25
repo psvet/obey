912e3ed3567775bc38afb9e0efc1d2461032892e
Kent Safranski <kent@fluidbyte.net>
2016-09-25 10:34:04 -0500

    fix(phone): Loosen default validation, issue #48 (#52)
    * fix(phone): Loosen default validation, issue #48
    
    * fix(circleci): fix artifacts on ci
    
    * fix(coverage): Replace isparta with babel-istanbul

--- test/src/typeStrategies/phone.spec.js
+++ test/src/typeStrategies/phone.spec.js
@@ -11,12 +11,18 @@ describe('type:phone', () => {
     expect(context.fail).to.be.calledWith('Value must be a valid phone number')
   })
   it('does not call context fail if value is a valid phone number', () => {
-    const context = {
-      value: '(555)-123-4567',
-      fail: sinon.spy()
-    }
-    phone.default(context)
-    expect(context.fail).to.not.be.called
+    // Variation
+    const contextOne = { value: '(555)-123-4567', fail: sinon.spy() }
+    phone.default(contextOne)
+    expect(contextOne.fail).to.not.be.called
+    // Variation
+    const contextTwo = { value: '555 123-4567', fail: sinon.spy() }
+    phone.default(contextTwo)
+    expect(contextTwo.fail).to.not.be.called
+    // Variation
+    const contextThree = { value: '5551234567', fail: sinon.spy() }
+    phone.default(contextThree)
+    expect(contextThree.fail).to.not.be.called
   })
   it('calls context.fail if value is not a valid numeric phone number', () => {
     const context = {

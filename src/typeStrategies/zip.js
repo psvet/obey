aa8e54b2ceda6ccad715b2b68f3be1062ea51fc0
Peter Svetlichny <petersvetlichny@gmail.com>
2018-03-28 21:02:30 -0700

    refactor(zip): replace default with US regex

--- src/typeStrategies/zip.js
+++ src/typeStrategies/zip.js
@@ -1,16 +1,10 @@
 const zip = {
   _regex: {
-    default: /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/,
-    us: /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/,
+    default: /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/,
     ca: /^([A-Z][0-9][A-Z])\s*([0-9][A-Z][0-9])$/
   },
   default: context => {
     if (context.value === null || context.value && !context.value.toString().match(zip._regex.default)) {
-      context.fail('Value must be a valid zip code')
-    }
-  },
-  us: context => {
-    if (context.value === null || context.value && !context.value.toString().match(zip._regex.us)) {
       context.fail('Value must be a valid US zip code')
     }
   },

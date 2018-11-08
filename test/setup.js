e086e5ff9d1a7efbcb8a423c72b809570830cc1d
Peter Svetlichny <petersvetlichny@gmail.com>
2018-11-07 21:33:31 -0800

    chore: upgrade dependencies

--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,7 @@ const chai = require('chai')
 const chaiaspromised = require('chai-as-promised')
 const sinon = require('sinon')
 const schai = require('sinon-chai')
-const module = require('module')
+const mod = require('module')
 global.sinon = sinon
 global.expect = chai.expect
 chai.use(schai)
@@ -12,4 +12,4 @@ chai.use(chaiaspromised)
 
 // Allow import relative to root
 process.env.NODE_PATH = path.join(__dirname, '..') + path.delimiter + (process.env.NODE_PATH || '')
-module._initPaths()
+mod._initPaths()

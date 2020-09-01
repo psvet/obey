e4ac7874c0e674b24d85262dff210458d91209b3
Peter Svetlichny <petersvetlichny@gmail.com>
2020-09-01 15:04:57 -0700

    refactor(uuid): allow upper and lowercase uuids by default

--- src/typeStrategies/uuid.js
+++ src/typeStrategies/uuid.js
@@ -1,10 +1,22 @@
 const uuid = {
   _regex: {
-    default: /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
+    default: /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+    upper: /^[0-9A-F]{8}-[0-9A-F]{4}-[1-5][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/,
+    lower: /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/,
   },
   default: context => {
     if (context.value == null || !context.value.length || !context.value.toString().match(uuid._regex.default)) {
       context.fail('Value must be a valid UUID')
+    }
+  },
+  upper: context => {
+    if (context.value == null || !context.value.length || !context.value.toString().match(uuid._regex.upper)) {
+      context.fail('Value must be a valid UUID with all uppercase letters')
+    }
+  },
+  lower: context => {
+    if (context.value == null || !context.value.length || !context.value.toString().match(uuid._regex.lower)) {
+      context.fail('Value must be a valid UUID with all lowercase letters')
     }
   }
 }
--- test/src/typeStrategies/uuid.spec.js
+++ test/src/typeStrategies/uuid.spec.js
@@ -1,28 +1,106 @@
 const uuid = require('src/typeStrategies/uuid')
 
 describe('type:uuid', () => {
-  it('calls context.fail if type is not a valid UUID', () => {
-    const context = {
-      value: 'foo',
-      fail: jest.fn()
-    }
-    uuid.default(context)
-    expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID')
+  describe('default', () => {
+    it('calls context.fail if type is not a valid UUID', () => {
+      const context = {
+        value: 'foo',
+        fail: jest.fn()
+      }
+      uuid.default(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID')
+    })
+    it('calls context.fail if type is empty', () => {
+      const context = {
+        value: '',
+        fail: jest.fn()
+      }
+      uuid.default(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID')
+    })
+    it('does not call context.fail if type is a valid UUID', () => {
+      const context = {
+        value: 'ef3d56e1-6046-4743-aa69-b94bc9e66181',
+        fail: jest.fn()
+      }
+      uuid.default(context)
+      expect(context.fail).not.toHaveBeenCalled()
+    })
+    it('allows uppercase characters', () => {
+      const context = {
+        value: 'EF3D56E1-6046-4743-AA69-B94BC9E66181',
+        fail: jest.fn()
+      }
+      uuid.default(context)
+      expect(context.fail).not.toHaveBeenCalled()
+    })
   })
-  it('calls context.fail if type is empty', () => {
-    const context = {
-      value: '',
-      fail: jest.fn()
-    }
-    uuid.default(context)
-    expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID')
+  describe('upper', () => {
+    it('calls context.fail if type is not a valid UUID', () => {
+      const context = {
+        value: 'foo',
+        fail: jest.fn()
+      }
+      uuid.upper(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID with all uppercase letters')
+    })
+    it('calls context.fail if type is empty', () => {
+      const context = {
+        value: '',
+        fail: jest.fn()
+      }
+      uuid.upper(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID with all uppercase letters')
+    })
+    it('does not call context.fail if type is a valid uppercase UUID', () => {
+      const context = {
+        value: 'EF3D56E1-6046-4743-AA69-B94BC9E66181',
+        fail: jest.fn()
+      }
+      uuid.upper(context)
+      expect(context.fail).not.toHaveBeenCalled()
+    })
+    it('calls context.fail if type is not an uppercase UUID', () => {
+      const context = {
+        value: 'ef3d56e1-6046-4743-aa69-b94bc9e66181',
+        fail: jest.fn()
+      }
+      uuid.upper(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID with all uppercase letters')
+    })
   })
-  it('does not call context.fail if type is a valid UUID', () => {
-    const context = {
-      value: 'ef3d56e1-6046-4743-aa69-b94bc9e66181',
-      fail: jest.fn()
-    }
-    uuid.default(context)
-    expect(context.fail).not.toHaveBeenCalled()
+  describe('lower', () => {
+    it('calls context.fail if type is not a valid UUID', () => {
+      const context = {
+        value: 'foo',
+        fail: jest.fn()
+      }
+      uuid.lower(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID with all lowercase letters')
+    })
+    it('calls context.fail if type is empty', () => {
+      const context = {
+        value: '',
+        fail: jest.fn()
+      }
+      uuid.lower(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID with all lowercase letters')
+    })
+    it('does not call context.fail if type is a valid lowercase UUID', () => {
+      const context = {
+        value: 'ef3d56e1-6046-4743-aa69-b94bc9e66181',
+        fail: jest.fn()
+      }
+      uuid.lower(context)
+      expect(context.fail).not.toHaveBeenCalled()
+    })
+    it('calls context.fail if type is not a lowercase UUID', () => {
+      const context = {
+        value: 'EF3D56E1-6046-4743-AA69-B94BC9E66181',
+        fail: jest.fn()
+      }
+      uuid.lower(context)
+      expect(context.fail).toHaveBeenCalledWith('Value must be a valid UUID with all lowercase letters')
+    })
   })
 })
